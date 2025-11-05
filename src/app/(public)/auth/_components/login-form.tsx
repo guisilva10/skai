9f1846028225e3ce18f26a7028142ef507ca96ab
guisilva10 <guisilva.070104@gmail.com>
2025-11-05 11:11:34 -0300

    feat: add login with credentials

--- src/app/(public)/auth/_components/login-form.tsx
+++ src/app/(public)/auth/_components/login-form.tsx
@@ -68,9 +68,12 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <Form {...form}>
-            <form className="p-6 md:p-8" onSubmit={form.handleSubmit(onSubmit)}>
-              <FieldGroup>
+          <FieldGroup className="space-y-6 px-6 py-6">
+            <Form {...form}>
+              <form
+                className="space-y-6"
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
                 <div className="flex flex-col items-center gap-2 text-center">
                   <h1 className="text-2xl font-bold">Seja bem vinda(o)</h1>
                   <p className="text-muted-foreground text-balance">
@@ -108,32 +111,31 @@ export function LoginForm({
                 <Field>
                   <Button type="submit">Login</Button>
                 </Field>
-                <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-                  Ou continue com
-                </FieldSeparator>
-                <Field className="grid grid-cols-1 gap-4">
-                  <Button
-                    variant="outline"
-                    type="button"
-                    disabled={isPending}
-                    onClick={handleGoogleSignIn}
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <path
-                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    <span className="sr-only">Login with Google</span>
-                  </Button>
-                </Field>
-                <FieldDescription className="text-center">
-                  Não possuí uma conta ?{" "}
-                  <a href="/auth/register">Cadastre-se</a>
-                </FieldDescription>
-              </FieldGroup>
-            </form>
-          </Form>
+              </form>
+            </Form>
+            <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+              Ou continue com
+            </FieldSeparator>
+            <Field className="grid grid-cols-1 gap-4">
+              <Button
+                variant="outline"
+                type="button"
+                disabled={isPending}
+                onClick={handleGoogleSignIn}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                    fill="currentColor"
+                  />
+                </svg>
+                <span className="sr-only">Login with Google</span>
+              </Button>
+            </Field>
+            <FieldDescription className="text-center">
+              Não possuí uma conta ? <a href="/auth/register">Cadastre-se</a>
+            </FieldDescription>
+          </FieldGroup>
           <div className="bg-muted relative hidden md:block">
             <img
               src="/woman-with-glowing-healthy-skin-applying-skincare-.jpg"
